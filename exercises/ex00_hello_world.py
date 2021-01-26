c1a7108b52a5f8354b123d54ffd42897897b80db
Your Name <chrissym@live.unc.edu>
2021-01-25 23:52:14 -0500

    Finished Exercises 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,6 +1,5 @@
 """My first program for COMP110."""
 
-_author_ = "730135317"
-print("Hello, world.")
-git config --global user.email "chrissym@live.unc.edu"
-git config --global user.name "Chrissy Bolesky"
+_author_="730135317"
+
+print("Hello,world.")
